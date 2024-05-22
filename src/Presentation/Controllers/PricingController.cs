d01be80d8b4efcf66cea0fd41e08a0907b0b39d7
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 15:07:55 -0300

    #orderbook add Controller

---
+++ src/Presentation/Controllers/PricingController.cs
@@ -0,0 +1,17 @@
+﻿using BitstampOrderBookService.src.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BitstampOrderBookService.src.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PricingController : ControllerBase
+    {
+        private readonly PricingService _pricingService;
+
+        public PricingController(PricingService pricingService)
+        {
+            _pricingService = pricingService;
+        }
+    }
+}
