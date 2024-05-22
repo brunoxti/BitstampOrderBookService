a1a70b7e8688fcac5003061210164ad1bf7299ba
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 14:49:15 -0300

    #orderBook add entities

---
+++ src/Domain/Entities/Order.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BitstampOrderBookService.src.Domain.Entities
+{
+    public class Order
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal Quantity { get; private set; }
+        public string Pair { get; private set; }
+
+        public Order()
+        {
+        }
+
+        public Order(decimal price, decimal quantity, string pair)
+        {
+            Price = price;
+            Quantity = quantity;
+            Pair = pair;
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantity} {Pair} @ {Price} USD";
+        }
+    }
+}
---
+++ src/Domain/Entities/OrderBook.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BitstampOrderBookService.src.Domain.Entities
+{
+    public class OrderBook
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; private set; }
+        public string Pair { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public List<Order> Asks { get; private set; }
+        public List<Order> Bids { get; private set; }
+
+        public OrderBook()
+        {
+            Asks = new List<Order>();
+            Bids = new List<Order>();
+        }
+
+        public OrderBook(string pair, DateTime timestamp)
+        {
+            Pair = pair;
+            Timestamp = timestamp;
+            Asks = new List<Order>();
+            Bids = new List<Order>();
+        }
+
+        public void AddAsk(Order ask)
+        {
+            Asks.Add(ask);
+        }
+
+        public void AddBid(Order bid)
+        {
+            Bids.Add(bid);
+        }
+     
+        public IReadOnlyList<Order> GetAsks() => Asks.AsReadOnly();
+        public IReadOnlyList<Order> GetBids() => Bids.AsReadOnly();
+    }
+}
