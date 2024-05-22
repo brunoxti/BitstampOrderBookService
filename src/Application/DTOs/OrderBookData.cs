c9b4dcf5ffe6c0d9ab2ba96a09fea8522e086808
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 14:50:06 -0300

    #orderbook add DTOS of orderBook

---
+++ src/Application/DTOs/OrderBookData.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BitstampOrderBookService.src.Application.DTOs
+{
+    public class OrderBookData
+    {
+        [JsonPropertyName("timestamp")]
+        public string Timestamp { get; set; }
+
+        [JsonPropertyName("microtimestamp")]
+        public string MicroTimestamp { get; set; }
+
+        [JsonPropertyName("bids")]
+        public List<string[]> Bids { get; set; }
+
+        [JsonPropertyName("asks")]
+        public List<string[]> Asks { get; set; }
+    }
+}
---
+++ src/Application/DTOs/OrderBookUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BitstampOrderBookService.src.Application.DTOs
+{
+
+    public class OrderBookUpdate
+    {
+        [JsonPropertyName("event")]
+        public string Event { get; set; }
+
+        [JsonPropertyName("channel")]
+        public string Channel { get; set; }
+
+        [JsonPropertyName("data")]
+        public OrderBookData Data { get; set; }
+    }
+}
