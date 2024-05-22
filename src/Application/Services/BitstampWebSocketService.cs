f309314b4e9329852b7d371aef1649caf8ae3f03
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 15:05:42 -0300

    #orderbook add BitstampWebSocketService

---
+++ src/Application/Services/BitstampWebSocketService.cs
@@ -0,0 +1,23 @@
+﻿using BitstampOrderBookService.src.Application.DTOs;
+using BitstampOrderBookService.src.Application.Interfaces;
+using BitstampOrderBookService.src.Domain.Entities;
+using MongoDB.Driver;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+
+namespace BitstampOrderBookService.src.Application.Services
+{
+    public class BitstampWebSocketService : IBitstampWebSocketService
+    {
+        private ClientWebSocket _webSocket;
+        private readonly IMongoCollection<OrderBook> _orderBookCollection;
+        private readonly ConcurrentDictionary<string, OrderBook> _orderBooks = new();
+
+        public BitstampWebSocketService(IMongoDatabase database)
+        {
+            _orderBookCollection = database.GetCollection<OrderBook>("orderbooks");
+        }
+    }
+}
