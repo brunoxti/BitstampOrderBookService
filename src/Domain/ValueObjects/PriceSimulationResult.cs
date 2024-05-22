d89844f6778dea2e2eef78493bc8684ca74e6708
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 14:57:20 -0300

    #orderbook add valueObjects

---
+++ src/Domain/ValueObjects/PriceSimulationResult.cs
@@ -0,0 +1,19 @@
+﻿using BitstampOrderBookService.src.Domain.Entities;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BitstampOrderBookService.src.Domain.ValueObjects
+{
+
+    public class PriceSimulationResult
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public Guid Id { get; set; }
+        public string Pair { get; set; }
+        public string Operation { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal TotalCost { get; set; }
+        public List<Order> OrdersUsed { get; set; }
+    }
+}
---
+++ src/Domain/ValueObjects/Statistics.cs
@@ -0,0 +1,11 @@
+﻿namespace BitstampOrderBookService.src.Domain.ValueObjects
+{
+    public sealed class Statistics
+    {
+        public decimal? Highest { get; set; }
+        public decimal? Lowest { get; set; }
+        public decimal? Average { get; set; }
+        public decimal? AccumulatedAverage { get; set; }
+        public decimal? AccumulatedQuantityAverage { get; set; }
+    }
+}
