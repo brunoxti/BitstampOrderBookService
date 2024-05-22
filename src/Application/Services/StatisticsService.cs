2e4ce37b2fe60ab657137c7b3101e81a00ffdac4
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 15:07:00 -0300

    #orderbook add StatisticsService

---
+++ src/Application/Services/StatisticsService.cs
@@ -0,0 +1,18 @@
+﻿using BitstampOrderBookService.src.Application.Interfaces;
+using BitstampOrderBookService.src.Domain.Entities;
+using BitstampOrderBookService.src.Domain.ValueObjects;
+using BitstampOrderBookService.src.Infrastructure.Data;
+using MongoDB.Driver;
+
+namespace BitstampOrderBookService.src.Application.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly IMongoCollection<OrderBook> _orderBookCollection;
+
+        public StatisticsService(IMongoDatabase database)
+        {
+            _orderBookCollection = database.GetCollection<OrderBook>("orderbooks");
+        }
+    }
+}
