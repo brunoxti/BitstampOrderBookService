79f08ddfefafb3b7b139df3952e15c4976ee6dda
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 15:06:24 -0300

    #orderbook add PricingService

---
+++ src/Application/Services/PricingService.cs
@@ -0,0 +1,19 @@
+﻿using BitstampOrderBookService.src.Application.Interfaces;
+using BitstampOrderBookService.src.Domain.Entities;
+using BitstampOrderBookService.src.Domain.ValueObjects;
+using BitstampOrderBookService.src.Infrastructure.Data;
+using MongoDB.Driver;
+using System.Diagnostics.Metrics;
+
+namespace BitstampOrderBookService.src.Application.Services
+{
+    public class PricingService : IPricingService
+    {
+        private readonly IMongoCollection<OrderBook> _orderBookCollection;
+
+        public PricingService(IMongoDatabase database)
+        {
+            _orderBookCollection = database.GetCollection<OrderBook>("orderbooks");
+        }
+    }
+}
