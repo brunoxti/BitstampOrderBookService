a0abb582cea6c27bf978a0ece06de97f5598ee6e
Bruno Alves <bruno.xti@outlook.com>
2024-05-22 15:01:48 -0300

    #orderbook add repositories from orderbook

---
+++ src/Infrastructure/Repository/IOrderBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using BitstampOrderBookService.src.Domain.Entities;
+
+namespace BitstampOrderBookService.src.Infrastructure.Repository
+{
+    public interface IOrderBookRepository
+    {
+        Task<OrderBook> GetLatestOrderBookAsync(string pair);
+        Task InsertOrderBookAsync(OrderBook orderBook);
+    }
+}
---
+++ src/Infrastructure/Repository/OrderBookRepository.cs
@@ -0,0 +1,33 @@
+﻿using BitstampOrderBookService.src.Domain.Entities;
+using BitstampOrderBookService.src.Infrastructure.Data;
+using MongoDB.Driver;
+
+namespace BitstampOrderBookService.src.Infrastructure.Repository
+{
+    public class OrderBookRepository : IOrderBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderBookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task InsertOrderBookAsync(OrderBook orderBook)
+        {
+            await _context.OrderBooks.ReplaceOneAsync(
+                ob => ob.Pair == orderBook.Pair && ob.Timestamp == orderBook.Timestamp,
+                orderBook,
+                new ReplaceOptions { IsUpsert = true }
+            );
+        }
+
+        public async Task<OrderBook> GetLatestOrderBookAsync(string pair)
+        {
+            return await _context.OrderBooks
+                .Find(ob => ob.Pair == pair)
+                .SortByDescending(ob => ob.Timestamp)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
