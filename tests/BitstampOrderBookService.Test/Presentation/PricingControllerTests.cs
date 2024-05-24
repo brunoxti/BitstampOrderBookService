5b856e848e2f9e984f1b64de1ce1d0f39f574824
Bruno Alves <bruno.xti@outlook.com>
2024-05-24 13:27:01 -0300

    #orderbook update unit test controller

--- tests/BitstampOrderBookService.Test/Presentation/PricingControllerTests.cs
+++ tests/BitstampOrderBookService.Test/Presentation/PricingControllerTests.cs
@@ -1,11 +1,12 @@
-﻿using BitstampOrderBookService.Presentation.Controllers;
-using BitstampOrderBookService.Application.Interfaces;
+﻿using BitstampOrderBookService.Application.Interfaces;
+using BitstampOrderBookService.Domain.Entities;
 using BitstampOrderBookService.Domain.ValueObjects;
+using BitstampOrderBookService.Presentation.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using Xunit;
-using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace BitstampOrderBookService.Tests.UnitTests
 {
@@ -21,41 +22,57 @@ namespace BitstampOrderBookService.Tests.UnitTests
         }
 
         [Fact]
-        public async Task SimulateBestPrice_Should_ReturnOkResult_WithPriceSimulationResult()
+        public async Task GetAllSimulations_Should_ReturnOkResult_WithAllSimulations()
         {
             // Arrange
-            var operation = "buy";
-            var instrument = "btcusd";
-            var quantity = 1.0m;
-            var result = new PriceSimulationResult { TotalCost = 1000 };
+            var simulations = new List<PriceSimulationResult>
+            {
+                new PriceSimulationResult { Pair = "btcusd" },
+                new PriceSimulationResult { Pair = "ethusd" }
+            };
 
-            _mockPricingService.Setup(service => service.SimulatePriceAsync(instrument, operation, quantity))
-                               .ReturnsAsync(result);
+            _mockPricingService.Setup(service => service.GetAllSimulationsAsync())
+                .ReturnsAsync(simulations);
 
             // Act
-            var response = await _controller.SimulateBestPrice(operation, instrument, quantity);
+            var result = await _controller.GetAllSimulations();
 
             // Assert
-            var okResult = Assert.IsType<OkObjectResult>(response);
-            Assert.Equal(result, okResult.Value);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<PriceSimulationResult>>(okResult.Value);
+            Assert.Equal(2, returnValue.Count);
         }
 
         [Fact]
-        public async Task SimulateBestPrice_Should_ReturnBadRequest_WhenExceptionThrown()
+        public async Task SimulateBestPrice_Should_ReturnOkResult_WithSimulationResult()
         {
             // Arrange
-            var operation = "buy";
-            var instrument = "btcusd";
-            var quantity = 1.0m;
+            var simulationResult = new PriceSimulationResult { Pair = "btcusd" };
 
-            _mockPricingService.Setup(service => service.SimulatePriceAsync(instrument, operation, quantity))
-                               .ThrowsAsync(new Exception("Simulation error"));
+            _mockPricingService.Setup(service => service.SimulatePriceAsync("btcusd", "buy", 1))
+                .ReturnsAsync(simulationResult);
 
             // Act
-            var response = await _controller.SimulateBestPrice(operation, instrument, quantity);
+            var result = await _controller.SimulateBestPrice("buy", "btcusd", 1);
 
             // Assert
-            var badRequestResult = Assert.IsType<BadRequestObjectResult>(response);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<PriceSimulationResult>(okResult.Value);
+            Assert.Equal("btcusd", returnValue.Pair);
+        }
+
+        [Fact]
+        public async Task SimulateBestPrice_Should_ReturnBadRequest_OnException()
+        {
+            // Arrange
+            _mockPricingService.Setup(service => service.SimulatePriceAsync("btcusd", "buy", 1))
+                .ThrowsAsync(new System.Exception("Simulation error"));
+
+            // Act
+            var result = await _controller.SimulateBestPrice("buy", "btcusd", 1);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal("Simulation error", badRequestResult.Value);
         }
     }
