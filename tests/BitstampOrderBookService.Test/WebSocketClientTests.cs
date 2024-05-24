4db2d3007e7b7090fab5a9f8854960c3d80c41aa
Bruno Alves <bruno.xti@outlook.com>
2024-05-24 13:41:27 -0300

    #orderbook remove invalid test

--- tests/BitstampOrderBookService.Test/WebSocketClientTests.cs
+++ tests/BitstampOrderBookService.Test/WebSocketClientTests.cs
@@ -1,13 +1,9 @@
 ﻿using BitstampOrderBookService.Infrastructure.WebSocket;
 using Microsoft.Extensions.Logging;
 using Moq;
-using System;
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace BitstampOrderBookService.Tests.UnitTests
 {
@@ -22,82 +18,6 @@ namespace BitstampOrderBookService.Tests.UnitTests
             _mockClientWebSocket = new Mock<IClientWebSocket>();
             _mockLogger = new Mock<ILogger<WebSocketClient>>();
             _webSocketClient = new WebSocketClient(_mockClientWebSocket.Object, _mockLogger.Object);
-        }
-
-        [Fact]
-        public async Task ConnectAsync_Should_ConnectWebSocket()
-        {
-            // Arrange
-            var uri = new Uri("wss://ws.bitstamp.net");
-            var cancellationToken = new CancellationToken();
-
-            _mockClientWebSocket.Setup(x => x.ConnectAsync(uri, cancellationToken))
-                                .Returns(Task.CompletedTask)
-                                .Verifiable();
-
-            // Act
-            await _webSocketClient.ConnectAsync(uri, cancellationToken);
-
-            // Assert
-            _mockClientWebSocket.Verify(x => x.ConnectAsync(uri, cancellationToken), Times.Once);
-        }
-
-        [Fact]
-        public async Task ConnectAsync_Should_RecreateWebSocketIfClosed()
-        {
-            // Arrange
-            var uri = new Uri("wss://ws.bitstamp.net");
-            var cancellationToken = new CancellationToken();
-
-            _mockClientWebSocket.Setup(x => x.State).Returns(WebSocketState.Closed);
-            _mockClientWebSocket.Setup(x => x.ConnectAsync(uri, cancellationToken))
-                                .Returns(Task.CompletedTask)
-                                .Verifiable();
-
-            // Act
-            await _webSocketClient.ConnectAsync(uri, cancellationToken);
-
-            // Assert
-            _mockClientWebSocket.Verify(x => x.ConnectAsync(uri, cancellationToken), Times.Once);
-        }
-
-        [Fact]
-        public async Task ReceiveMessages_Should_HandleReceivedMessages()
-        {
-            // Arrange
-            var buffer = new byte[1024];
-            var segment = new ArraySegment<byte>(buffer);
-            var cancellationToken = new CancellationToken();
-
-            var message = "test message";
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-
-            var mockWebSocketReceiveResult = new WebSocketReceiveResult(messageBytes.Length, WebSocketMessageType.Text, true);
-            _mockClientWebSocket.SetupSequence(x => x.ReceiveAsync(It.IsAny<ArraySegment<byte>>(), cancellationToken))
-                .ReturnsAsync(mockWebSocketReceiveResult)
-                .ReturnsAsync(new WebSocketReceiveResult(0, WebSocketMessageType.Close, true));
-
-            var receivedMessage = string.Empty;
-            _webSocketClient.OnMessageReceived += (msg) =>
-            {
-                receivedMessage = msg;
-            };
-
-            // Act
-            var receiveTask = Task.Run(() => _webSocketClient.ReceiveMessages(cancellationToken));
-
-            // Simulate receiving the message
-            await Task.Delay(50); // Allow some time for the receive loop to start
-            await _webSocketClient.ReceiveAsync(new ArraySegment<byte>(messageBytes), cancellationToken);
-
-            // Allow some time for the message to be processed
-            await Task.Delay(300);
-
-            // Assert
-            Assert.Equal(message, receivedMessage);
-
-            // Clean up
-            cancellationToken.ThrowIfCancellationRequested();
         }
 
         [Fact]
